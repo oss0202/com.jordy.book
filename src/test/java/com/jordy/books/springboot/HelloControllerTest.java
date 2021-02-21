b19253e90bf531f73887960c2fa1ef5ecc00d9a2
오성식 <oss0202@naver.com>
2021-02-21 18:42:52 +0900

    2장 테스트코드

--- src/test/java/com/jordy/books/springboot/HelloControllerTest.java
+++ src/test/java/com/jordy/books/springboot/HelloControllerTest.java
@@ -7,25 +7,25 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+//아래의 import 3개는 직접 작성 8)
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.jordy.books.springboot.web.HelloController;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@RunWith(SpringRunner.class) // 1)
+@WebMvcTest(controllers = HelloController.class) // 2)
 public class HelloControllerTest {
-    @Autowired
-    private MockMvc mvc;
+    @Autowired // 3)
+    private MockMvc mvc; // 4)
 
     @Test
-    public void returnHello() throws Exception{
+    public void hello가_리턴된다() throws Exception{
         String hello = "hello";
 
-        mvc.perform(get("/hello2"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(hello));
-
+        mvc.perform(get("/hello")) // 5)
+                .andExpect(status().isOk()) // 6)
+                .andExpect(content().string(hello)); // 7)
     }
 }

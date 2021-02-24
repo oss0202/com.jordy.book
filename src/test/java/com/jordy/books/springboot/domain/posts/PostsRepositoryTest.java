4d0e1f6a51d184f0d001f140255c0fbd540ff162
오성식 <oss0202@naver.com>
2021-02-24 23:45:14 +0900

    3장 JPA 3.3 Spring Data JPA 테스트 코드 작성하기

---
+++ src/test/java/com/jordy/books/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.jordy.books.springboot.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        //given
+        String title = "테스트게시글";
+        String content = "테스트본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("jordy@gmail.com")
+                .build());
+        //when
+        List<Posts> postsList = postsRepository.findAll();//
+
+        //then
+        Posts posts = postsList.get(0);
+        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+        Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+}

f988d4e58cddd1b2ee88b696da626bf6dc9cf7f0
오성식 <oss0202@naver.com>
2021-02-28 00:26:20 +0900

    Chapter 3 CRUD API

--- src/main/java/com/jordy/books/springboot/service/posts/PostsService.java
+++ src/main/java/com/jordy/books/springboot/service/posts/PostsService.java
@@ -1,4 +1,19 @@
 package com.jordy.books.springboot.service.posts;
 
+import com.jordy.books.springboot.domain.posts.PostsRepository;
+import com.jordy.books.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
 public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
 }
--- src/main/java/com/jordy/books/springboot/web/PostsApiController.java
+++ src/main/java/com/jordy/books/springboot/web/PostsApiController.java
@@ -1,4 +1,20 @@
 package com.jordy.books.springboot.web;
 
+import com.jordy.books.springboot.service.posts.PostsService;
+import com.jordy.books.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
 public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
 }

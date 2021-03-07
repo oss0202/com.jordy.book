f1762bc05781e4d15d60a5dbee57fa16c63e9107
오성식 <oss0202@naver.com>
2021-03-07 23:48:46 +0900

    Chapter 4 Mustache, Test

---
+++ src/main/java/com/jordy/books/springboot/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.jordy.books.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String Index(){
+        return "index";
+    }
+}

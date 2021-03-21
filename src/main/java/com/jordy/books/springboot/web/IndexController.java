2449e835f8f232ddb94ea12a93ee50bbf6dfc579
Oh <oss0202@naver.com>
2021-03-21 15:46:06 +0900

    Chapter 4 index, posts-save add

--- src/main/java/com/jordy/books/springboot/web/IndexController.java
+++ src/main/java/com/jordy/books/springboot/web/IndexController.java
@@ -1,14 +1,21 @@
 package com.jordy.books.springboot.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String Index(){
-        System.out.println("index call");
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
     }
 }

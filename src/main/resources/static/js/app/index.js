4157734e6bcd425e71029505fde62a74be0d32f8
오성식 <oss0202@naver.com>
2021-04-07 23:23:14 +0900

    Chapter 4 posts-delete

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -8,6 +8,10 @@ var main = {
         $('#btn-update').on('click', function (){ // (1)
             _this.update();
         })
+
+        $('#btn-delete').on('click', function (){
+            _this.delete();
+        })
     }
     ,save : function () {
         var data = {
@@ -46,6 +50,21 @@ var main = {
         }).done(function () {
             window.location.href = '/';
         }).fail(function (error) {
+            alert(JSON.stringify(error));
+        })
+    }
+    ,delete : function (){
+        var id = $('#id').val();
+
+        $.ajax({
+            type : 'DELETE'
+            ,url : '/api/v1/posts/' + id
+            ,dataType : 'json'
+            ,contentType : 'application/json; charset=utf-8'
+        }).done(function (){
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error){
             alert(JSON.stringify(error));
         })
     }

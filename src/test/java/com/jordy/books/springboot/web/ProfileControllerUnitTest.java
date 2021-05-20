f0b7be3e3af48a774df55027f0730915b3e3c164
Oh <oss0202@naver.com>
2021-05-20 19:02:54 +0900

    Chapter 10 profile add

---
+++ src/test/java/com/jordy/books/springboot/web/ProfileControllerUnitTest.java
@@ -0,0 +1,58 @@
+package com.jordy.books.springboot.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.springframework.mock.env.MockEnvironment;
+
+public class ProfileControllerUnitTest {
+    @Test
+    public void real_profile_조회된다(){
+        //given
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        Assertions.assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void real_profile이_없으면_첫_번째가_조회된다(){
+        //given
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        ///when
+        String profile = controller.profile();
+
+        //then
+        Assertions.assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void active_profile이_없으면_default_가_조회된다(){
+        //given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        Assertions.assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
